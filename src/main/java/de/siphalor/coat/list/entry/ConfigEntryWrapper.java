37b1b26d8dda3fde0eabababe3ac7522e9f9dba1
Siphalor <info@siphalor.de>
2021-06-26 17:21:30 +0200

    Remove unused class

--- src/main/java/de/siphalor/coat/list/entry/ConfigEntryWrapper.java
+++
@@ -1,8 +0,0 @@
-package de.siphalor.coat.list.entry;
-
-public interface ConfigEntryWrapper {
-	String getDescription();
-	default String getTooltipDescription() {
-		return getDescription();
-	}
-}
